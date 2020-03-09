cdfbc65f6c57c7ec3d9d07055243f4c3abd7494a
AlexFedczuk <alexfed1011@gmail.com>
2020-03-09 15:01:51 -0300

    Lo practicado el lunes 09/03/2020

--- tres.js
+++ tres.js
@@ -7,7 +7,107 @@ e) El promedio de edad entre los hombres solteros.*/
 
 function mostrar()
 {	
-	var nombrePasajero;
+	var nombreDelPasajero;
+	var edadDelPasajero;
+	var sexoDelPasajero;
+	var estadoCivilDelPasajero;
+	var respuesta;
+	var nombreDelHombreCasadoMasJoven;
+	var edadDelPasajeroMasJoven;
+	var contador;
+	var sexoDelPasajeroMasViejo;
+	var nombreDelPasajeroMasViejo;
+	var edadDelPasajeroMasViejo;
+	var contadorDeMujeresCasadas;
+	var promedioDeEdadEntreMujeres;
+	var contadorDeMujeres;
+	var acumuladorDeEdadDeMujeres;
+	var promedioDeEdadEntreHombresSolteros;
+	var acumuladorDeEdadEntreHombresSolteros;
+	var contadorDeEdadEntreHombresSolteros
+
+
+	contador=0;
+	contadorDeMujeresCasadas=0;
+	contadorDeMujeres=0;
+	acumuladorDeEdadDeMujeres=0;
+	contadorDeEdadEntreHombresSolteros=0;
+	acumuladorDeEdadEntreHombresSolteros=0;
+
+	do
+	{
+		do
+		{
+			nombreDelPasajero=prompt("Ingrese el nombre del pasajero.");
+		}
+		while(!isNaN(nombreDelPasajero));
+
+		do
+		{
+			edadDelPasajero=prompt("Ingrese la edad del pasajero.");
+			edadDelPasajero=parseInt(edadDelPasajero);
+		}
+		while(isNaN(edadDelPasajero) || edadDelPasajero<18);
+
+		do
+		{
+			sexoDelPasajero=prompt("Ingrese el sexo del pasajero.");
+		}
+		while(!isNaN(sexoDelPasajero) || sexoDelPasajero!="f" && sexoDelPasajero!="m");
+
+		do
+		{
+			estadoCivilDelPasajero=prompt("Ingrese el estado civil del pasajero.");
+		}
+		while(!isNaN(estadoCivilDelPasajero) || estadoCivilDelPasajero!="soltero" && estadoCivilDelPasajero!="casado" && estadoCivilDelPasajero!="viudo");
+
+		if(contador==0 && estadoCivilDelPasajero=="casado" && sexoDelPasajero=="m" || edadPasajeroMasJoven>edadDelPasajero && estadoCivilDelPasajero=="casado" && sexoDelPasajero=="m");
+		{
+			nombreDelHombreCasadoMasJoven=nombreDelPasajero;
+			edadDelPasajeroMasJoven=edadDelPasajero;
+		}
+
+		if(contador==0 || edadDelPasajeroMasViejo<edadDelPasajero);
+		{
+			nombreDelPasajeroMasViejo=nombreDelPasajero;
+			edadDelPasajeroMasViejo=edadDelPasajero;
+			sexoDelPasajeroMasViejo=sexoDelPasajero;
+		}
+
+		if(sexoDelPasajero=="f" && estadoCivilDelPasajero=="casado");
+		{
+			contadorDeMujeresCasadas++;	
+		}
+
+		if(sexoDelPasajero=="f")
+		{
+			contadorDeMujeres++;
+			acumuladorDeEdadDeMujeres=acumuladorDeEdadDeMujeres+edadDelPasajero;
+		}
+
+		if(sexoDelPasajero=="m" && estadoCivilDelPasajero=="soltero")
+		{
+			contadorDeEdadEntreHombresSolteros++;
+			acumuladorDeEdadEntreHombresSolteros=acumuladorDeEdadEntreHombresSolteros+edadDelPasajero;
+		}
+
+		contador++;
+
+		respuesta=prompt("Ingrese si, para seguir reservando.");
+	}
+	while(respuesta=="si");
+
+	promedioDeEdadEntreMujeres=acumuladorDeEdadDeMujeres/contadorDeMujeres;
+	promedioDeEdadEntreHombresSolteros=acumuladorDeEdadEntreHombresSolteros/contadorDeEdadEntreHombresSolteros;
+
+    document.write("<br> El nombre del hombre casado más joven es: "+nombreDelHombreCasadoMasJoven);
+    document.write("<br> El sexo y nombre del pasajero/a más viejo es: "+sexoDelPasajeroMasViejo+" y "+nombreDelPasajeroMasViejo);
+    document.write("<br> La cantidad de mujeres que hay casadas es: "+contadorDeMujeresCasadas);
+    document.write("<br> El promedio de edad entre las mujeres es: "+promedioDeEdadEntreMujeres);
+    document.write("<br> El promedio de edad entre los hombres solteros es: "+promedioDeEdadEntreHombresSolteros);
+}
+
+	/*var nombrePasajero;
 	var edadPasajero;
 	var sexoPasajero;
 	var estadoCivilPasajero;
@@ -17,7 +117,7 @@ function mostrar()
 	var edadPasajeroMasJoven;
 	var edadPasajeroMasViejo;
 	var sexoPasajeroMasViejo;
-	var nombrePasajeroMasViejo;
+	var nombreDelPasajeroMasViejo;
 
 	bandera=0;
 
@@ -59,7 +159,7 @@ function mostrar()
 		if(bandera==0 || edadPasajeroMasViejo<edadPasajero)
 		{
 			edadPasajeroMasViejo=edadPasajero;
-			nombrePasajeroMasViejo=nombrePasajero;
+			nombreDelPasajeroMasViejo=nombrePasajero;
 			sexoPasajeroMasViejo=sexoPasajero;
 		}
 	
@@ -68,6 +168,5 @@ function mostrar()
 	while(respuesta=="si");
 
 	document.write("<br> El nombre del hombre casado más joven es: "+nombreHombreCasadoMasJoven);
-	document.write("<br> El nombre del pasajero mas viejo es: "+nombrePasajeroMasViejo+" y su sexo es: "+sexoPasajeroMasViejo);
-}
+	document.write("<br> El nombre del pasajero mas viejo es: "+nombreDelPasajeroMasViejo+" y su sexo es: "+sexoPasajeroMasViejo);*/
 
